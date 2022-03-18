c8f7dad45482ed0dd4ec3da4c338d2b79f5433d5
wgp88 <wgp88@att.net>
2022-03-18 10:26:08 -0400

    Model & Animations added

--- Festival-of-Dionysus/Assets/Scripts/ThirdPersonCameraController.cs
+++ Festival-of-Dionysus/Assets/Scripts/ThirdPersonCameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ThirdPersonCameraController : MonoBehaviour
 {
 	public float Speed;
+	public Animator PlayerAnimator;
 
 	private void Update()
 	{
@@ -16,6 +17,47 @@ public class ThirdPersonCameraController : MonoBehaviour
 			float ver = Input.GetAxis("Vertical");
 			Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
 			transform.Translate(playerMovement, Space.Self);
+
+			if(Input.GetAxis("Vertical") > 0)
+            {
+				PlayerAnimator.ResetTrigger("Stop");
+				PlayerAnimator.ResetTrigger("Right");
+				PlayerAnimator.ResetTrigger("Left");
+				PlayerAnimator.ResetTrigger("Backward");
+				PlayerAnimator.SetTrigger("Forward");
+            }
+			if (Input.GetAxis("Vertical") < 0)
+			{
+				PlayerAnimator.ResetTrigger("Stop");
+				PlayerAnimator.ResetTrigger("Right");
+				PlayerAnimator.ResetTrigger("Left");
+				PlayerAnimator.ResetTrigger("Forward");
+				PlayerAnimator.SetTrigger("Backward");
+			}
+			else if (Input.GetAxis("Horizontal") > 0)
+			{
+				PlayerAnimator.ResetTrigger("Stop");
+				PlayerAnimator.ResetTrigger("Forward");
+				PlayerAnimator.ResetTrigger("Backward");
+				PlayerAnimator.ResetTrigger("Left");
+				PlayerAnimator.SetTrigger("Right");
+			}
+			else if (Input.GetAxis("Horizontal") < 0)
+			{
+				PlayerAnimator.ResetTrigger("Stop");
+				PlayerAnimator.ResetTrigger("Forward");
+				PlayerAnimator.ResetTrigger("Backward");
+				PlayerAnimator.ResetTrigger("Right");
+				PlayerAnimator.SetTrigger("Left");
+			}
+			else if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            {
+				PlayerAnimator.ResetTrigger("Forward");
+				PlayerAnimator.ResetTrigger("Backward");
+				PlayerAnimator.ResetTrigger("Right");
+				PlayerAnimator.ResetTrigger("Left");
+				PlayerAnimator.SetTrigger("Stop");
+			}
 		}
 	}
 }
