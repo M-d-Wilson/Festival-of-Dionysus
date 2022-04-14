24d54c61f481adccfe8c9b6eebc94be9c96d5cf8
wgp88 <wgp88@att.net>
2022-04-13 20:43:23 -0400

    3D & 2D Assets implemented
    Added the player avatar and animations again as well as sprites for Maze minigame and Medusa minigame.

--- Festival-of-Dionysus/Assets/Scripts/ThirdPersonCharacterController.cs
+++ Festival-of-Dionysus/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ThirdPersonCharacterController : MonoBehaviour
 {
 	public float speed;
+	public Animator PlayerAnimator;
 
 	void Update()
 	{
@@ -17,6 +18,47 @@ public class ThirdPersonCharacterController : MonoBehaviour
 		float ver = Input.GetAxis("Vertical");
 		Vector3 playerMovement = new Vector3(hor, 0f, ver) * speed * Time.deltaTime;
 		transform.Translate(playerMovement, Space.Self);
+
+		if(Input.GetAxis("Vertical") > 0)
+        {
+			PlayerAnimator.ResetTrigger("Idle");
+			PlayerAnimator.ResetTrigger("Right");
+			PlayerAnimator.ResetTrigger("Left");
+			PlayerAnimator.ResetTrigger("Backward");
+			PlayerAnimator.SetTrigger("Forward");
+		}
+		if (Input.GetAxis("Vertical") < 0)
+		{
+			PlayerAnimator.ResetTrigger("Idle");
+			PlayerAnimator.ResetTrigger("Right");
+			PlayerAnimator.ResetTrigger("Left");
+			PlayerAnimator.ResetTrigger("Forward");
+			PlayerAnimator.SetTrigger("Backward");
+		}
+		if (Input.GetAxis("Horizontal") > 0)
+		{
+			PlayerAnimator.ResetTrigger("Idle");
+			PlayerAnimator.ResetTrigger("Forward");
+			PlayerAnimator.ResetTrigger("Backward");
+			PlayerAnimator.ResetTrigger("Left");
+			PlayerAnimator.SetTrigger("Right");
+		}
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			PlayerAnimator.ResetTrigger("Idle");
+			PlayerAnimator.ResetTrigger("Forward");
+			PlayerAnimator.ResetTrigger("Backward");
+			PlayerAnimator.ResetTrigger("Right");
+			PlayerAnimator.SetTrigger("Left");
+		}
+		if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+		{
+			PlayerAnimator.ResetTrigger("Left");
+			PlayerAnimator.ResetTrigger("Forward");
+			PlayerAnimator.ResetTrigger("Backward");
+			PlayerAnimator.ResetTrigger("Right");
+			PlayerAnimator.SetTrigger("Idle");
+		}
 	}
 
 
